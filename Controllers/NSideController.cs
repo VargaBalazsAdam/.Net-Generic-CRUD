35a25eb04ec4cb57282d3888db7b574eb3cabca4
Varga Balázs Ádám <112722847+VargaBalazsAdam@users.noreply.github.com>
2023-09-26 15:10:57 +0200

    Controllers

--- Controllers/NSideController.cs
+++ Controllers/NSideController.cs
@@ -1,10 +1,63 @@
 namespace Generic_CRUD.Controllers
 {
+  [Route("api/[controller]")]
+  [ApiController]
   public class NSideController : Controller
   {
-    public IActionResult Index()
+    private readonly NSideRepository repository;
+    private readonly DataContext dataContext;
+    private readonly IConfiguration _configuration;
+    public NSideController(IConfiguration configuration)
     {
-      return View();
+      _configuration = configuration;
+      dataContext = new(_configuration);
+      repository = new(dataContext);
+    }
+
+    [HttpPost("create")]
+    public IActionResult Create(ModelNameN model)
+    {
+      return this.Run(() =>
+      {
+        return Ok(repository.Insert(model));
+      });
+    }
+
+    [HttpGet("get")]
+    public IActionResult Get(int id)
+    {
+      return this.Run(() =>
+      {
+        return Ok(repository.GetByKey(id));
+      });
+    }
+
+    [HttpGet("get-all")]
+    public IActionResult GetAll()
+    {
+      return this.Run(() =>
+      {
+        return Ok(repository.GetAll());
+      });
+    }
+
+    [HttpPatch("update")]
+    public IActionResult Update(ModelNameN model)
+    {
+      return this.Run(() =>
+      {
+        return Ok(repository.Update(model));
+      });
+    }
+
+    [HttpDelete("delete")]
+    public IActionResult Delete(int id)
+    {
+      return this.Run(() =>
+      {
+        repository.Delete(id);
+        return Ok("Succesfully deleted.");
+      });
     }
   }
 }
